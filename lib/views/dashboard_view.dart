dfb6584980b3a6f914a2501a1d4b3ea5dc9f8e8e
moelsayed1 <mostafaembaby19@gmail.com>
2024-08-12 15:44:13 +0300

    dashboard_mobile_layout.dart done

--- lib/views/dashboard_view.dart
+++ lib/views/dashboard_view.dart
@@ -1,17 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dashboard/views/widgets/adaptive_layout_widget.dart';
+import 'package:responsive_dashboard/views/widgets/custom_drawer.dart';
 import 'package:responsive_dashboard/views/widgets/dashboard_desktop_layout.dart';
+import 'package:responsive_dashboard/views/widgets/dashboard_mobile_layout.dart';
 import 'package:responsive_dashboard/views/widgets/dashboard_tablet_layout.dart';
 
-class DashboardView extends StatelessWidget {
+class DashboardView extends StatefulWidget {
   const DashboardView({super.key});
+
+  @override
+  State<DashboardView> createState() => _DashboardViewState();
+}
+
+class _DashboardViewState extends State<DashboardView> {
+
+  final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xffE5E5E5),
+      key: scaffoldKey,
+      appBar: MediaQuery.of(context).size.width < 800
+          ? AppBar(
+              elevation: 0,
+              backgroundColor: const Color(0xffFAFAFA),
+              leading: IconButton(
+                onPressed: () {
+                  scaffoldKey.currentState!.openDrawer();
+                },
+                icon: const Icon(
+                  Icons.menu_outlined,
+                ),
+              ),
+            )
+          : null,
+      drawer: MediaQuery.sizeOf(context).width < 800 ? const CustomDrawer() : null,
+      backgroundColor: const Color(0xfff7f9fa),
       body: AdaptiveLayout(
-        mobileLayout: (context) => const SizedBox(),
+        mobileLayout: (context) => const DashboardMobileLayout(),
         tabletLayout: (context) => const DashboardTabletLayout(),
         desktopLayout: (context) => const DashboardDesktopLayout(),
       ),
--- lib/views/widgets/custom_drawer.dart
+++ lib/views/widgets/custom_drawer.dart
@@ -12,8 +12,9 @@ class CustomDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      width: MediaQuery.sizeOf(context).width * 0.7,
       color: Colors.white,
-      child: CustomScrollView(
+      child: const CustomScrollView(
         slivers: [
           SliverToBoxAdapter(
             child: UserInfoListTile(
